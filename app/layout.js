ab8de750b798b6e22b2cdb5f17a316f21697ecc8
Kunal Pandey <kunalkp94@gmail.com>
2025-03-24 20:20:44 -0400

    speed insight tag added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const outFit = Outfit({
   subsets: ["latin"],
@@ -25,6 +26,8 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Analytics />
+
+        <SpeedInsights />
       </body>
     </html>
   );
